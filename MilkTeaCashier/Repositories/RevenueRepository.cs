401472b6ca583ef63f4546690d03601a50856179
LQVinh11022004 <146046130+LeQuangVinh11022004@users.noreply.github.com>
2024-07-21 22:46:19 +0700

    revenue

---
+++ MilkTeaCashier/Repositories/RevenueRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    
+    public class RevenueRepository
+    {
+        private MilkTeaCashierContext _context;
+
+        public List<BillInfo> GetAll()
+        {
+            _context = new();
+            return _context.BillInfos.Include("IdBillNavigation").ToList();
+        }
+
+        public void Delete(BillInfo x)
+        {
+            _context = new();
+            _context.BillInfos.Remove(x);
+            _context.SaveChanges();
+        }
+    }
+}
